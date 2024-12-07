3bf0445ea706e785be6b9e3e5ae81690bc82bad7
Najeeriii <adrianomcle08@gmail.com>
2024-12-07 17:34:48 -0500

    Add files via upload

---
+++ Swap.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the First Number"))
+b=int(input("Enter the Second Number"))
+c=int(input("Enter the Third Number"))
+print("The actual Numbers",a,b,c)
+a,b,c=b,c,a
+print("The numbers after the swapping",a,b,c)
